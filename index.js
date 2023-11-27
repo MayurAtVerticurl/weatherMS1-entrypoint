d02e453796b658c69ffc959e15b017b068212e1d
MayurAtVerticurl <144439561+MayurAtVerticurl@users.noreply.github.com>
2023-11-27 17:26:33 +0530

    env Update

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3000;
-
 
 const app = express();
 
 app.use(helmet());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json()); //accepting only json data
 // app.use(express.urlencoded());  //accept url encoded data
 
-const ms1URL = `https://selfassuredmiserablerectangles--mayurrajan.repl.co/getWeatherData`;
-const ms2URL = `https://sociablewhirlwindmicrostation--mayurrajan.repl.co/getAiReport`;
+const ms1URL =
+  process.env.ms1URL ||
+  `https://selfassuredmiserablerectangles--mayurrajan.repl.co/getWeatherData`;
+const ms2URL =
+  process.env.ms2URL ||
+  `https://sociablewhirlwindmicrostation--mayurrajan.repl.co/getAiReport`;
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    up: '1'
-  })
+    up: "1",
+  });
   // res.json(
   //   {
   //     temperature: 26.88,
@@ -35,18 +37,18 @@ app.get('/', (req, res) => {
   //     gpt: 'The weather risk level in Periyanayakkanpalaiyam is relatively low with only haze as the main weather condition. However, visibility is reduced to 5000 meters, posing some risk while moving around.'
   //   }
   // )
+});
 
-})
-
-app.get('/getWeatherReport', async (req, res, next) => {
+app.get("/getWeatherReport", async (req, res, next) => {
   try {
     // Extract latitude and longitude from request parameters
     const { lat, lon } = req.query;
 
     if (!lat || !lon) {
-      return res.status(400).json({ error: 'Latitude and longitude are required in the request parameters.' });
+      return res.status(400).json({
+        error: "Latitude and longitude are required in the request parameters.",
+      });
     }
-
 
     const response1 = await axios.get(ms1URL, {
       params: {
@@ -56,7 +58,7 @@ app.get('/getWeatherReport', async (req, res, next) => {
     });
 
     const ms1DATA = response1.data;
-    
+
     const response2 = await axios.get(ms2URL, {
       params: {
         data: JSON.stringify(ms1DATA),
@@ -76,23 +78,14 @@ app.get('/getWeatherReport', async (req, res, next) => {
       sunset: ms1DATA.sys.sunset,
       sunrise: ms1DATA.sys.sunrise,
       gpt: ms2DATA.gpt,
-    }
-
-
+    };
 
     res.json(output);
     return;
-
-
   } catch (error) {
-
     next(error);
-
   }
-
-})
-
-
+});
 
 app.use((error, req, res, next) => {
   if (error.status) {
@@ -103,12 +96,10 @@ app.use((error, req, res, next) => {
   res.json({
     message: error.message,
     error: "something went wrong",
-    stack: error.stack
-  })
-})
-
-
+    stack: error.stack,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
-})
+});
